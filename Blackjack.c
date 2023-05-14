5727906c79f541ebc5ce8fb99359976d21d77ddf
sehako <dhtpgkr1999@gmail.com>
2023-05-14 15:27:49 +0900

    블랙잭 깃허브

--- Blackjack.c
+++ Blackjack.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <asm/ioctls.h>
-#include <sys/signal.h>
-#include <unistd.h>
-#include <termios.h>
 
+int main(void) {
+
+    return 0;
+}
