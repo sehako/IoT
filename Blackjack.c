640d2d2ee0898490e874a4d0ff3fb5e52c1bc8f9
excodaw <dhtpgkr1999@gmail.com>
2023-05-15 12:06:12 +0900

    블랙잭 파일을 이용한 배팅

--- Blackjack.c
+++ Blackjack.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
 
 int main(void) {
+    long money;
+    FILE* file;
+    file = fopen("money.txt", "r");
+    fscanf(file, "%ld", &money);
+    fclose(file);
 
     return 0;
 }
