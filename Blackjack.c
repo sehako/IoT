38a4f2a397664316439a615b7e3bad2681884151
excodaw <dhtpgkr1999@gmail.com>
2023-05-17 21:16:25 +0900

    블랙잭 배팅 시스템 구현, 1주차 발표 마크 다운

--- Blackjack.c
+++ Blackjack.c
@@ -4,12 +4,24 @@
 #include <time.h>
 #include <string.h>
 
+void finish(long);
+
 int main(void) {
     long money;
     FILE* file;
-    file = fopen("money.txt", "r");
+
+    file = fopen("money.txt", "r"); //배팅 금액이 저장된 파일을 불러옴
     fscanf(file, "%ld", &money);
     fclose(file);
 
+    finish(money);
+    
     return 0;
+}
+
+void finish(long num) {
+    FILE* file;
+    file = fopen("money.txt", "w"); //배팅 금액이 저장된 파일을 불러옴
+    fprintf(file, "%ld", num);
+    fclose(file);
 }
