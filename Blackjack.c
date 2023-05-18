abc9d4c9fe58d76ca67046e3e9e69fbd6c258b0d
excodaw <dhtpgkr1999@gmail.com>
2023-05-18 09:44:24 +0900

    코드 수정

--- Blackjack.c
+++ Blackjack.c
@@ -3,25 +3,39 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
+#include <stdbool.h>
 
 void finish(long);
 
 int main(void) {
     long money;
+    long bet_money;
+    bool win;
     FILE* file;
 
-    file = fopen("money.txt", "r"); //배팅 금액이 저장된 파일을 불러옴
+    file = fopen("money.txt", "r"); //배팅 금액이 저장된 파일을 불러    옴
     fscanf(file, "%ld", &money);
     fclose(file);
 
+    scanf("%ld", &bet_money);
+
+    // 게임 실행 코드 구현 예정
+
+    if(win) {
+        bet_money *= 2;
+        money += bet_money;
+    }
+    else {
+        money -= bet_money;
+    }
+
     finish(money);
-    
     return 0;
 }
 
 void finish(long num) {
     FILE* file;
-    file = fopen("money.txt", "w"); //배팅 금액이 저장된 파일을 불러옴
+    file = fopen("money.txt", "w"); 
     fprintf(file, "%ld", num);
     fclose(file);
 }
