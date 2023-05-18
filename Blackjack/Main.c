f75f3b0f6056c2fa9159cedb69078da0e67ddd76
excodaw <dhtpgkr1999@gmail.com>
2023-05-18 17:58:00 +0900

    블랙잭 70% 구현

--- Blackjack/Main.c
+++ Blackjack/Main.c
@@ -6,15 +6,19 @@
 #include <string.h>
 #include <stdbool.h>
 
-void Wallet(bool, long, long*);
 void Finish(bool, long);
 void DeckGen(int [4][13]);
 int Draw(char* , char* , int[4][13]);
+bool CheckBlackJack(int);
+bool Fight(int, int);
 
 int main(void) {
-    long money;
+    long money = 0;
     long bet_money = 0;
     int arr[4][13]; //행 부분은 카드 문양 열 부분은 카드 값
+    int play_check = 1;
+    int hit_check = 1;
+    int user_card, dealer_card;
     char alpha, alphabet, shape, dealer_shape;
 
     bool win; //승리 여부
@@ -26,22 +30,87 @@ int main(void) {
     fscanf(file, "%ld", &money);
     fclose(file);
 
+    printf("%ld\n", money);
+
     //게임 실행 부분
-    while (loop) {
+    while (true) {
+        int user_score = 0;
+        int dealer_score = 0;
+
         DeckGen(&arr); //덱 생성 함수
-        int user_card = Draw(&shape, &alpha, arr);
-        int dealer_card = Draw(&dealer_shape, &alphabet, arr);
 
-        printf("%c %c\n", shape, dealer_shape);
-        printf("%d %d\n", user_card, dealer_card);
-        printf("%c %c\n", alpha, alphabet);
-
-        // deck_gen(deck); 덱 생성 함수
         //해당 입력을 다이얼 버튼으로 받을 예정
+        printf("배팅금 입력(소지액: %ld)\n", money);
         scanf("%ld", &bet_money);
 
-        //배팅 금액 계산 함수
-        Wallet(false, bet_money, &money);
+        // 처음 게임 시작 시 카드 분배 구현
+        user_card = Draw(&shape, &alpha, arr);
+        dealer_card = Draw(&dealer_shape, &alphabet, arr);
+        user_score += user_card;
+        dealer_score += dealer_card;
+        printf("%c, %d\n", shape, user_card);
+        user_card = Draw(&shape, &alpha, arr);
+        dealer_card = Draw(&dealer_shape, &alphabet, arr);
+        printf("%c, %d\n", shape, user_card);
+        printf("%c, %d\n", shape, dealer_card);
+        user_score += user_card;
+        dealer_score += dealer_card;
+
+        //블랙잭 체크 함수
+        if (CheckBlackJack(user_score)) {
+            printf("%ld\n", user_score);
+            printf("BlackJack!!\n");
+            money += bet_money * 2;
+            continue;
+        }
+        //푸시 채크 부분
+        else if (CheckBlackJack(user_score) && CheckBlackJack(dealer_score)) {
+            printf("%ld\n", user_score);
+            printf("%ld\n", dealer_score);
+            printf("Push\n");
+        }
+        else {
+            while (true) {
+                printf("%ld\n", user_score);
+                printf("1 | Hit   0 | Stand\n");
+                scanf("%d", &hit_check);
+                if (hit_check == 1) {
+                    user_score += Draw(&shape, &alpha, arr);
+                }
+                else {
+                    break;
+                }
+                if (user_score > 21) {
+                    printf("%ld\n", user_score);
+                    printf("Busted!\n");
+                    money -= bet_money;
+                    break;
+                }
+            }
+        }
+
+        //딜러가 가진 카드의 합이 16이하인 경우 드로우하는 규칙 구현
+        while (dealer_score <= 16) {
+            dealer_score += Draw(&dealer_shape, &alphabet, arr);
+        }
+
+        //승패 계산 부분
+        if(dealer_score > 21) {
+            printf("Dealer busted\n");
+            money += bet_money * 2;
+        }
+        else if(user_score > dealer_score) {
+            printf("You win!\n");
+            money += bet_money * 2;
+        }
+        else if(user_score == dealer_score) {
+            printf("Push\n");
+            money += bet_money * 2;
+        }
+        else {
+            printf("You lose!\n");
+            money -= bet_money;
+        }
 
         //배팅금 전부 소진 시 게임 오버 출력 후 배팅금 5000원 충전 & 종료
         if (money == 0) {
@@ -51,31 +120,18 @@ int main(void) {
         }
 
         //사용자 종료 시 계산된 최종 배팅금을 파일에 저장
-        if (bet_money == 0) {
+        printf("재도전? (0입력 시 종료)\n");
+        scanf("%d", &play_check);
+        if (play_check == 0) {
+            printf("GameFinished\n");
             Finish(false, money);
             break;
         }
-    }
-
-    //전체 덱 출력 테스트
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 13; j++) {
-            printf("%d ", arr[i][j]);
+        else {
+            continue;
         }
-        printf("\n");
     }
     return 0;
-}
-
-// 승패에 따른 배팅금 처리
-void Wallet(bool game, long betting, long* result) {
-    if (game) {
-        betting *= 2;
-        *result += betting;
-    }
-    else {
-        *result -= betting;
-    }
 }
 
 //게임 종료 or 오버 시 파일 처리
@@ -106,26 +162,66 @@ void DeckGen(int arr[4][13]) {
 //카드 드로우 함수 구현
 int Draw(char* shape_pt, char* alpha_pt, int arr[4][13]) {
     int draw = 0;
-    srand(time(NULL));
-
-    while (draw < 10) {
+    srand(time(NULL)); //랜덤 정수 생성
+    while (true) {
         int shape = rand() % 4;
         int card = rand() % 13;
 
         if (arr[shape][card] != 0) {
-            if (arr[shape][card] >= 11) {
-                draw = 10;
-                arr[shape][card] = 0;
-            }
-            else {
-                draw = arr[shape][card];
-                arr[shape][card] = 0;
+            switch (shape) {
+            case 0:
+                *shape_pt = 'S';
+                break;
+            case 1:
+                *shape_pt = 'D';
+                break;
+            case 2:
+                *shape_pt = 'C';
+                break;
+            default:
+                *shape_pt = 'H';
                 break;
             }
+            draw = arr[shape][card];
+            arr[shape][card] = 0;
+            break;
         }
         else {
             continue;
         }
     }
+
+    if (draw >= 11) {
+        switch (draw) {
+        case 11:
+            *alpha_pt = 'J';
+            break;
+        case 12:
+            *alpha_pt = 'Q';
+            break;
+        default:
+            *alpha_pt = 'K';
+            break;
+        }
+        draw = 10;
+    }
     return draw;
+}
+
+bool CheckBlackJack(int user_sum) {
+    if (user_sum == 21) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool Fight(int user, int dealer) {
+    if (user > dealer) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
