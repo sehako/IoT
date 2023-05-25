856b2e7d2d3adf260e8f450b943e1046de90c33a
excodaw <dhtpgkr1999@gmail.com>
2023-05-25 16:05:52 +0900

    블랙잭 수정 에이스 처리 구문 구현

--- BlackJack.c
+++ BlackJack.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,11 +7,11 @@
 #include <string.h>
 #include <stdbool.h>
 
-void ResetHand(char[10], char[10]);
+
+void ResetHand(char*, char*);
 int Betting(); //배팅을 딥 스위치로 구현할 함수
-int Draw(char* , char* , int arr[4][13]);
-void AceCheck(char*, int*, int*);
-void CardShow(char* , char* , int); //뽑은 카드를 Dot Matrix와 디지털 패드로 구현할 함수
+int Draw(char*, char*, char (*)[13], char*, int*);
+void CardShow(char*, char*, int); //뽑은 카드를 Dot Matrix와 디지털 패드로 구현할 함수
 bool HandCheck(char*);
 void Finish(bool, long);
 
@@ -20,8 +21,8 @@ int main(void) {
     bool user_busted = false;
     int user_card, dealer_card, first_dealer_card, user_score, dealer_score, user_count, dealer_count;
     char alpha, dealer_alpha, shape, dealer_shape, first_dealer_alpha, first_dealer_shape;
-    char user_hand[10] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-    char dealer_hand[10] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char user_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+    char dealer_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
     FILE* file;
 
     //배팅 금액이 저장된 파일 로드
@@ -29,93 +30,94 @@ int main(void) {
     fscanf(file, "%ld", &money);
     fclose(file);
 
-    while(true) { //게임 실행 반복문
+    while (true) { //게임 실행 반복문
         user_score = 0;
         dealer_score = 0;
         user_count = 0;
         dealer_count = 0;
 
         //딥 스위치로 배팅금 입력
+        printf("배팅금 입력 %ld\n", money);
+        scanf("%ld", &bet_money);
+        money -= bet_money;
 
-        int arr[4][13] = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
-        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
-        }; //행 부분은 카드 문양 열 부분은 카드 값
+        char deck[4][13] = { {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'},
+        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'}, 
+        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'}, 
+        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'}};//행 부분은 카드 문양 열 부분은 카드 값
 
         //첫 번째 카드 분배
-        user_card = Draw(&shape, &alpha, arr);
-        dealer_card = Draw(&dealer_shape, &dealer_alpha, arr);
-        AceCheck(&alpha, user_hand, &user_count);
-        AceCheck(&dealer_alpha, dealer_hand, &dealer_count);
-        user_score += user_card;
-        dealer_score += dealer_card;
-        CardShow(&shape, &alpha, &user_card);
-        sleep(3);
-        first_dealer_card = dealer_card;
+        user_score += Draw(&shape, &alpha, deck, user_hand, &user_count);
+        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand, &dealer_count);
         first_dealer_alpha = dealer_alpha;
         first_dealer_shape = dealer_shape;
-        
-        //두 번째 카드 분배
-        user_card = Draw(&shape, &alpha, arr);
-        dealer_card = Draw(&dealer_shape, &dealer_alpha, arr);
-        AceCheck(&alpha, user_hand, &user_count);
-        AceCheck(&dealer_alpha, dealer_hand, &dealer_count);
-        CardShow(&shape, &alpha, user_card);
-        sleep(3);
-        CardShow(&dealer_shape, &dealer_alpha, dealer_card);
-        user_score += user_card;
-        dealer_score += dealer_card;
 
-        if(user_score == 21) {
+        //두 번째 카드 분배
+        user_score += Draw(&shape, &alpha, deck, user_hand, &user_count);
+        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand, &dealer_count);
+
+        //에이스 두 장 뽑으면 자동으로 11, 1로 취급
+        if (user_score > 21) {
+            HandCheck(user_hand);
+            user_score -= 10;
+        }
+        if (dealer_score > 21) {
+            HandCheck(dealer_hand);
+            user_score -= 10;
+        }
+
+        if (user_score == 21) {
             //블랙잭 구현 부분
             money += bet_money * 2;
             continue;
         }
         else {
-            while(true) {
+            while (true) {
+                int check = 0;
                 //힛 or 스탠드 구현
-                //int or char hit: 딥 스위치로 입력받을 변수
-                if(user_score > 21) {
-                    if(HandCheck(user_hand)) user_score -= 10;
+                scanf("%d", &check);
+                if (user_score > 21) {
+                    if (HandCheck(user_hand)) user_score -= 10;
                     else {
                         user_busted = true;
                         break;
-                    } 
+                    }
                 }
-
-                if(true) {
-                    user_card = Draw(&shape, &alpha, arr);
-                    CardShow(&shape, &alpha, user_card, &user_score);
+                if (check == 1) {
+                    user_score += Draw(&shape, &alpha, deck, user_hand, &user_count);  
                 }
-                else {
-                    break;
-                }
+                else break;
             }
         }
-        if(user_busted) {
-            user_busted = false;
-        }
+        if (user_busted) user_busted = false;
         else {
-            CardShow(&first_dealer_shape, &first_dealer_alpha, first_dealer_card);
-            
+            bool dealer_bust = false;
+
             //딜러가 가진 카드의 합이 16이하인 경우 딜러는 무조건 드로우
-            while(dealer_score <= 16) {
-                dealer_card = Draw(&dealer_shape, &dealer_alpha, arr);
-                CardShow(&dealer_shape, &dealer_alpha, dealer_card);
-                sleep(3);
+            while (dealer_score <= 16) {
+                dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand, &dealer_count);
+
+                if (dealer_score > 21) {
+                    if (HandCheck(dealer_hand)) {
+                        dealer_score -= 10;
+                    }
+                    else {
+                        dealer_bust = true;
+                        break;
+                    }
+                }
             }
 
             //승패 계산
-            if(dealer_score > 21) {
+            if (dealer_bust) {
                 //딜러 버스트 부분 LCD 구현
                 money += bet_money * 2;
             }
-            else if(user_score > dealer_score) {
+            else if (user_score > dealer_score) {
                 //플레이어 승리 부분 LCD 구현
                 money += bet_money * 2;
             }
-            else if(user_score == dealer_score) {
+            else if (user_score == dealer_score) {
                 //무승부 부분 LCD 구현
                 money += bet_money;
             }
@@ -124,16 +126,14 @@ int main(void) {
                 printf("You lose!\n");
             }
         }
-        if(money == 0) {
+        if (money == 0) {
             //소지금 전부 소진 시
             Finish(true, money);
             break;
         }
 
         //재시작 부분
-        if (true) {
-            continue;
-        }
+        if (true) continue;
         else {
             //사용자 종료 시 계산된 최종 배팅금을 파일에 저장
             Finish(false, money);
@@ -145,91 +145,81 @@ int main(void) {
 
 //손패 초기화
 void ResetHand(char arr[10], char ar[10]) {
-    for(int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
         arr[i] = ' ';
         ar[i] = ' ';
     }
 }
 
 //덱 드로우 함수
-int Draw(char* shape_pt, char* alpha_pt, int arr[4][13]) {
-    int draw = 0;
-    srand(time(NULL)); //랜덤 정수 생성
+int Draw(char* shape_pt, char* alpha_pt, char (*arr)[13], char* hand, int* count) {
+    int value = 0;
     while (true) {
+        srand(time(NULL));
         int shape = rand() % 4;
+        srand(time(NULL));
         int card = rand() % 13;
 
-        if (arr[shape][card] != 0) { //0값이 아닌 배열이 나올때까지 반복
+        if (arr[shape][card] != ' ') {
+            *alpha_pt = arr[shape][card];
+            hand[*count] = arr[shape][card];
+            *count++;
+            arr[shape][card] = ' ';
             switch (shape) {
             case 0:
-                *shape_pt = '♠';
+                *shape_pt = 'S';
                 break;
             case 1:
-                *shape_pt = '◆';
+                *shape_pt = 'C';
                 break;
             case 2:
-                *shape_pt = '♣';
+                *shape_pt = 'D';
                 break;
             default:
-                *shape_pt = '♥';
+                *shape_pt = 'H';
                 break;
             }
-            draw = arr[shape][card];
-            arr[shape][card] = 0; //뽑힌 배열값을 0으로 변경(중복 방지)
+            switch (card) {
+            case 0:
+                value = 11;
+                break;
+            case 10:
+                value = 10;
+                break;
+            case 11:
+                value = 10;
+                break;
+            case 12:
+                value = 10;
+                break;
+            default:
+                value = card + 1;
+                break;
+            }
             break;
         }
-        else {
-            continue;
-        }
     }
-
-    switch (draw) {
-        case 0:
-            *alpha_pt = 'A';
-            draw = 11;
-            break;
-        case 11:
-            *alpha_pt = 'J';
-            draw = 10;
-            break;
-        case 12:
-            *alpha_pt = 'Q';
-            draw = 10;
-            break;
-        default:
-            *alpha_pt = 'K';
-            draw = 10;
-            break;
-    }
-    return draw;
-}
-
-void AceCheck(char* alphabet, int* arr, int* arr_count) {
-    if(*alphabet == 'A') {
-        arr[*arr_count] = 'A';
-        *arr_count++;
-        *alphabet = ' ';
-    }
+    return value;
 }
 
 //Dot Matrix로 카드 구현
 void CardShow(char* shape, char* alpha, int card) {
-    switch(*alpha) {
-        case 'K':
+    switch (*alpha) {
+    case 'K':
         *alpha = ' ';
         break;
-        case 'Q':
+    case 'Q':
         *alpha = ' ';
         break;
-        case 'J':
+    case 'J':
         *alpha = ' ';
         break;
     }
 }
 
 bool HandCheck(char* arr) {
-    for(int i = 0; i < 10; i++) {
-        if(arr[i] == 'A') {
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] == 'A') {
             arr[i] = ' ';
             return true;
         }
