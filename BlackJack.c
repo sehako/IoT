2647d073bfcb315eb03f822ae934c4ea1cc61db4
excodaw <dhtpgkr1999@gmail.com>
2023-05-25 16:36:21 +0900

    블랙잭 에이스 처리 수정, 손패 초기화 시기 수정,

--- BlackJack.c
+++ BlackJack.c
@@ -10,8 +10,8 @@
 
 void ResetHand(char*, char*);
 int Betting(); //배팅을 딥 스위치로 구현할 함수
-int Draw(char*, char*, char (*)[13], char*, int*);
-void CardShow(char*, char*, int); //뽑은 카드를 Dot Matrix와 디지털 패드로 구현할 함수
+int Draw(char*, char*, char(*)[13], char*);
+void CardShow(char*, char*); //뽑은 카드를 Dot Matrix와 디지털 패드로 구현할 함수
 bool HandCheck(char*);
 void Finish(bool, long);
 
@@ -21,8 +21,6 @@ int main(void) {
     bool user_busted = false;
     int user_card, dealer_card, first_dealer_card, user_score, dealer_score, user_count, dealer_count;
     char alpha, dealer_alpha, shape, dealer_shape, first_dealer_alpha, first_dealer_shape;
-    char user_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-    char dealer_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
     FILE* file;
 
     //배팅 금액이 저장된 파일 로드
@@ -30,33 +28,38 @@ int main(void) {
     fscanf(file, "%ld", &money);
     fclose(file);
 
-    while (true) { //게임 실행 반복문
+    //게임 실행 반복문
+    while (true) {
+        //손패 초기화
+        char user_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        char dealer_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
         user_score = 0;
         dealer_score = 0;
-        user_count = 0;
-        dealer_count = 0;
 
         //딥 스위치로 배팅금 입력
         printf("배팅금 입력 %ld\n", money);
         scanf("%ld", &bet_money);
         money -= bet_money;
 
+        //행 부분은 카드 문양 열 부분은 카드 값
         char deck[4][13] = { {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'},
-        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'}, 
-        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'}, 
-        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'}};//행 부분은 카드 문양 열 부분은 카드 값
+        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'},
+        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'},
+        {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K', 'J'} };
 
         //첫 번째 카드 분배
-        user_score += Draw(&shape, &alpha, deck, user_hand, &user_count);
-        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand, &dealer_count);
+        user_score += Draw(&shape, &alpha, deck, user_hand);
+        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
+
+        //딜러의 첫 번째 카드 저장 플레이어와 승부시 출력할 용도
         first_dealer_alpha = dealer_alpha;
         first_dealer_shape = dealer_shape;
 
         //두 번째 카드 분배
-        user_score += Draw(&shape, &alpha, deck, user_hand, &user_count);
-        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand, &dealer_count);
+        user_score += Draw(&shape, &alpha, deck, user_hand);
+        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
 
-        //에이스 두 장 뽑으면 자동으로 11, 1로 취급
+        //초반 시작 부분에서 에이스 두 장 뽑으면 자동으로 11, 1로 취급
         if (user_score > 21) {
             HandCheck(user_hand);
             user_score -= 10;
@@ -84,7 +87,7 @@ int main(void) {
                     }
                 }
                 if (check == 1) {
-                    user_score += Draw(&shape, &alpha, deck, user_hand, &user_count);  
+                    user_score += Draw(&shape, &alpha, deck, user_hand);
                 }
                 else break;
             }
@@ -95,8 +98,7 @@ int main(void) {
 
             //딜러가 가진 카드의 합이 16이하인 경우 딜러는 무조건 드로우
             while (dealer_score <= 16) {
-                dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand, &dealer_count);
-
+                dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
                 if (dealer_score > 21) {
                     if (HandCheck(dealer_hand)) {
                         dealer_score -= 10;
@@ -143,16 +145,8 @@ int main(void) {
     return 0;
 }
 
-//손패 초기화
-void ResetHand(char arr[10], char ar[10]) {
-    for (int i = 0; i < 10; i++) {
-        arr[i] = ' ';
-        ar[i] = ' ';
-    }
-}
-
 //덱 드로우 함수
-int Draw(char* shape_pt, char* alpha_pt, char (*arr)[13], char* hand, int* count) {
+int Draw(char* shape_pt, char* alpha_pt, char(*arr)[13], char* hand) {
     int value = 0;
     while (true) {
         srand(time(NULL));
@@ -162,8 +156,12 @@ int Draw(char* shape_pt, char* alpha_pt, char (*arr)[13], char* hand, int* count
 
         if (arr[shape][card] != ' ') {
             *alpha_pt = arr[shape][card];
-            hand[*count] = arr[shape][card];
-            *count++;
+            for (int i = 0; i < 10; i++) {
+                if (hand[i] == ' ') {
+                    hand[i] = arr[shape][card];
+                    break;
+                }
+            }
             arr[shape][card] = ' ';
             switch (shape) {
             case 0:
@@ -203,18 +201,7 @@ int Draw(char* shape_pt, char* alpha_pt, char (*arr)[13], char* hand, int* count
 }
 
 //Dot Matrix로 카드 구현
-void CardShow(char* shape, char* alpha, int card) {
-    switch (*alpha) {
-    case 'K':
-        *alpha = ' ';
-        break;
-    case 'Q':
-        *alpha = ' ';
-        break;
-    case 'J':
-        *alpha = ' ';
-        break;
-    }
+void CardShow(char* shape, char* alpha) {
 }
 
 bool HandCheck(char* arr) {
