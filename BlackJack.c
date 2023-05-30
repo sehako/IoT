71feaaac91dfe08fb181f54e5ceb421529358886
excodaw <dhtpgkr1999@gmail.com>
2023-05-30 14:25:22 +0900

    수정본 4

--- BlackJack.c
+++ BlackJack.c
@@ -13,159 +13,6 @@
 #define dot "/dev/dot"
 #define fnd "/dev/fnd"
 
-long Betting(long); //배팅을 딥 스위치로 구현할 함수
-int Draw(char*, char*, char(*)[13], char*);
-void CardShow(char, char); //뽑은 카드를 Dot Matrix로 보여줄 함수
-void DealerCardShow(char, char);
-bool HandCheck(char*);
-void ResultPrint(int);
-void Finish(bool, long);
-
-int main(void) {
-    long money = 0;
-    long bet_money = 0;
-    bool user_busted = false;
-    int user_card, dealer_card, first_dealer_card, user_score, dealer_score, user_count, dealer_count;
-    char alpha, dealer_alpha, shape, dealer_shape, first_dealer_alpha, first_dealer_shape;
-    FILE* file;
-
-    //배팅 금액이 저장된 파일 로드
-    file = fopen("money.txt", "r");
-    fscanf(file, "%ld", &money);
-    fclose(file);
-
-    //fnd에 숫자를 띄우기위한 파일 로드 후 배열저장
-    int user_money[4] = {0};
-    file = fopen("money.txt", "r");
-    fscanf(file, "%1d%1d%1d%1d",&user_money[0], &user_money[1], &user_money[2], &user_money[3]);
-    fclose(file);
-
-    //게임 실행 반복문
-    while (true) {
-        //손패 초기화
-        char user_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-        char dealer_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-        user_score = 0;
-        dealer_score = 0;
-
-        //초기금액 3초간 표시
-        FND_control(user_money,3);
-        //LCD로 배팅금 입력 부분 출력
-        //딥 스위치로 배팅금 입력
-        // bet_money = Betting(money);
-        money -= bet_money;
-
-        //행 부분은 카드 문양 열 부분은 카드 값
-        unsigned char deck[4][13] = { 
-            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'},
-            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'},
-            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'},
-            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'} };
-
-        //첫 번째 카드 분배
-        user_score += Draw(&shape, &alpha, deck, user_hand);
-        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
-
-
-        //첫 번째 카드 출력(딜러는 LCD, 플레이어는 Dot Matrix)
-        DealerCardShow(dealer_shape, dealer_alpha);        
-        CardShow(shape, alpha);
-
-        //두 번째 카드 분배
-        user_score += Draw(&shape, &alpha, deck, user_hand);
-        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
-
-        //두 번째 카드 출력(플레이어만)
-        CardShow(shape, alpha);
-
-        //초반 시작 부분에서 에이스 두 장 뽑으면 자동으로 11, 1로 취급
-        if (user_score > 21) {
-            HandCheck(user_hand);
-            user_score -= 10;
-        }
-        if (dealer_score > 21) {
-            HandCheck(dealer_hand);
-            user_score -= 10;
-        }
-
-        if (user_score == 21) {
-            //블랙잭 구현 부분
-            money += bet_money * 2;
-            continue;
-        }
-        else {
-            while (true) {
-                int check = 0;
-                //힛 or 스탠드 구현
-                scanf("%d", &check);
-                if (user_score > 21) {
-                    if (HandCheck(user_hand)) user_score -= 10;
-                    else {
-                        user_busted = true;
-                        break;
-                    }
-                }
-                if (check == 1) {
-                    user_score += Draw(&shape, &alpha, deck, user_hand);
-                }
-                else break;
-            }
-        }
-        if (user_busted) user_busted = false;
-        else {
-            bool dealer_bust = false;
-
-            //딜러가 가진 카드의 합이 16이하인 경우 딜러는 무조건 드로우
-            while (dealer_score <= 16) {
-                dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
-                if (dealer_score > 21) {
-                    if (HandCheck(dealer_hand)) dealer_score -= 10;
-                    else {
-                        dealer_bust = true;
-                        break;
-                    }
-                }
-            }
-
-            //승패 계산
-            if (dealer_bust) {
-                //딜러 버스트 부분 LCD 구현
-                ResultPrint(0);
-                money += bet_money * 2;
-            }
-            else if (user_score > dealer_score) {
-                //플레이어 승리 부분 LCD 구현
-                ResultPrint(0);
-                money += bet_money * 2;
-            }
-            else if (user_score == dealer_score) {
-                //무승부 부분 LCD 구현
-                ResultPrint(1);
-                money += bet_money;
-            }
-            else {
-                //패배
-                ResultPrint(3);
-                printf("You lose!\n");
-            }
-        }
-        if (money == 0) {
-            ResultPrint(2);
-            //소지금 전부 소진 시
-            Finish(true, money);
-            break;
-        }
-
-        //재시작 부분
-        if (true) continue;
-        else {
-            //사용자 종료 시 계산된 최종 배팅금을 파일에 저장
-            Finish(false, money);
-            break;
-        }
-    }
-    return 0;
-}
 
 long Betting(long money) {
     long bet_money;
@@ -196,7 +43,7 @@ long Betting(long money) {
 }
 
 //덱 드로우 함수
-int Draw(char* shape_pt, char* alpha_pt, char(*arr)[13], char* hand) {
+int Draw(char* shape_pt, char* alpha_pt, char arr[4][13], char* hand) {
     int value = 0;
     while (true) {
         srand(time(NULL));
@@ -276,7 +123,7 @@ unsigned char mtn[13][8] = {
     {0x1C,0x08,0x08,0x08,0x08,0x48,0x48,0x30},  //J
     {0x38,0x44,0x82,0x82,0x82,0x8A,0x44,0x3A},  //Q
     {0x44,0x48,0x50,0x60,0x50,0x48,0x44,0x44}  //K
-    ;}
+    };
 
     //예외처리
     int dot_mtx;
@@ -431,7 +278,6 @@ void DealerCardShow(char shape, char alpha) {
         perror("open");
         exit(1);
     }
-    scanf("%s", buf);
     write(clcd_d, &buf, strlen(buf));
     close(clcd_d);
 }
@@ -493,3 +339,150 @@ void Finish(bool over, long num) {
     }
     fclose(file);
 }
+
+int main(void) {
+    long money = 0;
+    long bet_money = 0;
+    bool user_busted = false;
+    int user_card, dealer_card, first_dealer_card, user_score, dealer_score, user_count, dealer_count;
+    char alpha, dealer_alpha, shape, dealer_shape, first_dealer_alpha, first_dealer_shape;
+    FILE* file;
+
+    //배팅 금액이 저장된 파일 로드
+    file = fopen("money.txt", "r");
+    fscanf(file, "%ld", &money);
+    fclose(file);
+
+    //fnd에 숫자를 띄우기위한 파일 로드 후 배열저장
+    int user_money[4] = {0};
+    file = fopen("money.txt", "r");
+    fscanf(file, "%1d%1d%1d%1d",&user_money[0], &user_money[1], &user_money[2], &user_money[3]);
+    fclose(file);
+
+    //게임 실행 반복문
+    while (true) {
+        //손패 초기화
+        char user_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        char dealer_hand[10] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        user_score = 0;
+        dealer_score = 0;
+
+        //초기금액 3초간 표시
+        FND_control(user_money,3);
+        //LCD로 배팅금 입력 부분 출력
+        //딥 스위치로 배팅금 입력
+        // bet_money = Betting(money);
+        money -= bet_money;
+
+        //행 부분은 카드 문양 열 부분은 카드 값
+        unsigned char deck[4][13] = { 
+            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'},
+            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'},
+            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'},
+            {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'K', 'J'} };
+
+        //첫 번째 카드 분배
+        user_score += Draw(&shape, &alpha, deck, user_hand);
+        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
+
+
+        //첫 번째 카드 출력(딜러는 LCD, 플레이어는 Dot Matrix)
+        DealerCardShow(dealer_shape, dealer_alpha);        
+        CardShow(shape, alpha);
+
+        //두 번째 카드 분배
+        user_score += Draw(&shape, &alpha, deck, user_hand);
+        dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
+
+        //두 번째 카드 출력(플레이어만)
+        CardShow(shape, alpha);
+
+        //초반 시작 부분에서 에이스 두 장 뽑으면 자동으로 11, 1로 취급
+        if (user_score > 21) {
+            HandCheck(user_hand);
+            user_score -= 10;
+        }
+        if (dealer_score > 21) {
+            HandCheck(dealer_hand);
+            user_score -= 10;
+        }
+
+        if (user_score == 21) {
+            //블랙잭 구현 부분
+            money += bet_money * 2;
+            continue;
+        }
+        else {
+            while (true) {
+                int check = 0;
+                //힛 or 스탠드 구현
+                scanf("%d", &check);
+                if (user_score > 21) {
+                    if (HandCheck(user_hand)) user_score -= 10;
+                    else {
+                        user_busted = true;
+                        break;
+                    }
+                }
+                if (check == 1) {
+                    user_score += Draw(&shape, &alpha, deck, user_hand);
+                }
+                else break;
+            }
+        }
+        if (user_busted) user_busted = false;
+        else {
+            bool dealer_bust = false;
+
+            //딜러가 가진 카드의 합이 16이하인 경우 딜러는 무조건 드로우
+            while (dealer_score <= 16) {
+                dealer_score += Draw(&dealer_shape, &dealer_alpha, deck, dealer_hand);
+                if (dealer_score > 21) {
+                    if (HandCheck(dealer_hand)) dealer_score -= 10;
+                    else {
+                        dealer_bust = true;
+                        break;
+                    }
+                }
+            }
+
+            //승패 계산
+            if (dealer_bust) {
+                //딜러 버스트 부분 LCD 구현
+                ResultPrint(0);
+                money += bet_money * 2;
+            }
+            else if (user_score > dealer_score) {
+                //플레이어 승리 부분 LCD 구현
+                ResultPrint(0);
+                money += bet_money * 2;
+            }
+            else if (user_score == dealer_score) {
+                //무승부 부분 LCD 구현
+                ResultPrint(1);
+                money += bet_money;
+            }
+            else {
+                //패배
+                ResultPrint(3);
+                printf("You lose!\n");
+            }
+        }
+        if (money == 0) {
+            ResultPrint(2);
+            //소지금 전부 소진 시
+            Finish(true, money);
+            break;
+        }
+
+        //재시작 부분
+        if (true) continue;
+        else {
+            //사용자 종료 시 계산된 최종 배팅금을 파일에 저장
+            Finish(false, money);
+            break;
+        }
+    }
+    return 0;
+}
+
